db725e839059d4f9fdc89339761c6d09eb1b5942
Gilber Moreno <6ilberM@gmail.com>
2017-11-10 03:34:01 +1300

    It creates the file !

--- gilXmlp/gilXmlp/main.cpp
+++ gilXmlp/gilXmlp/main.cpp
@@ -1,11 +1,31 @@
+#include <iomanip>
 #include<iostream>
 #include<fstream>
-
-
+#include<string>
+using namespace std;
 int main()
 {
+	ifstream infile;
+	infile.open("GilXMLPARS/GilFavGames.xml");
+	//
+	if (infile.fail())
+	{
+		int temp;
+		cout << "Yo It failed?" << endl;
+		std::cin>> temp;
+		exit(1);
+	}
+	int n=1;
+	string s;
+	int itemp;
+	std::cin >> s;
 
+	ofstream outData;
+	outData.open("outfile..xlsx", ios::app);
+	outData << n<<endl;
+	outData << s << endl;
 
+	system("pause");
 
 	return 1;
 }
